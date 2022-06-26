37e831228deda7f09791417dc98d550976e9aff2
Lyons800 <oisinlyons13@gmail.com>
2022-06-26 11:36:40 -0400

    Initial Commit

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import "./PageContent.css";
 import logo from "~/assets/logo.svg";
 import browser from "webextension-polyfill";
+import IDLogo from "~/assets/BlkIDENTI3 Logo.png";
 
 
 function PageContent(props) {
-  const imageUrl = new URL(logo, import.meta.url).href;
+  const imageUrl = new URL(IDLogo, import.meta.url).href;
 
   const [count, setCount] = useState(0);
 
@@ -13,11 +14,8 @@ function PageContent(props) {
     <div>
       <img src={imageUrl} height="45" alt="" />
       <h1>{props.children}</h1>
-      <button type="button" onClick={() => setCount((count) => count + 1)}>
-        Count: {count}
-      </button>
       <button type="button" onClick={()=> { browser.runtime.openOptionsPage() }}>
-      Options
+      Open IDENTI3
       </button>
     </div>
   );
--- src/entries/contentScript/primary/App.jsx
+++ src/entries/contentScript/primary/App.jsx
@@ -1,12 +1,13 @@
 import logo from "~/assets/logo.svg";
 import "./App.css";
+import IDLogo from "~/assets/BlkIDENTI3 Logo.png"
 
 function App() {
   const logoImageUrl = new URL(logo, import.meta.url).href;
 
   return (
     <div className="logo">
-      <img src={logoImageUrl} height="50" alt="" />
+      <img src={IDLogo} height="50" alt="" />
     </div>
   );
 }
